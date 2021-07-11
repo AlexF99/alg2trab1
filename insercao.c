d2b63ddf90aac65a89c4068306b27d3b46ead81b
AlexF99 <alexandreopf@gmail.com>
2021-07-11 15:47:23 -0300

    implemented insertionsort

--- insercao.c
+++ insercao.c
@@ -1,9 +1,23 @@
 #include "biblioteca.h"
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] pelo método da seleção e devolve v */
+/* ordena v[a..b] pelo metodo da insercao e devolve v */
 
-int *insercao(int v[], unsigned int a, unsigned int b) {
+int *insercao(int v[], unsigned int a, unsigned int b)
+{
+  int length = b - a + 1;
+  int i = length - 1;
+
+  if (length <= 1)
+    return v;
+
+  insercao(v, a, b - 1);
+
+  while (i > 0 && compara(v[i], v[i - 1]) < 0)
+  {
+    troca(v, i, i - 1);
+    i--;
+  }
 
   return v;
 }

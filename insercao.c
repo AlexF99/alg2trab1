2fddb67e6a3ba6d0a71eafe3680c1fb5067e0eb2
AlexF99 <alexandreopf@gmail.com>
2021-07-12 19:44:58 -0300

    fixed insertionsort

--- insercao.c
+++ insercao.c
@@ -1,23 +1,33 @@
 #include "biblioteca.h"
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] pelo metodo da insercao e devolve v */
 
+int minimo(int v[], int a, int b)
+{
+  int ultimo = v[b];
+
+  for (int i = a; i < b; i++)
+    if (compara(v[i], ultimo) == 1)
+      return i;
+
+  return b;
+}
+
+void insere(int v[], int a, int b)
+{
+  int minIndex = minimo(v, a, b);
+
+  for (int i = b; i > minIndex; i--)
+    troca(v, i, i - 1);
+}
+
+//Ordena v[a..b] pelo método da seleção e devolve v.
 int *insercao(int v[], unsigned int a, unsigned int b)
 {
-  int length = b - a + 1;
-  int i = length - 1;
-
-  if (length <= 1)
-    return v;
-
-  insercao(v, a, b - 1);
-
-  while (i > 0 && compara(v[i], v[i - 1]) < 0)
+  if (a < b)
   {
-    troca(v, i, i - 1);
-    i--;
+    insercao(v, a, b - 1);
+    insere(v, a, b);
   }
-
   return v;
 }

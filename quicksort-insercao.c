f652a30a49c49c795a4ab71810ed7de5afa3ccca
AlexF99 <alexandreopf@gmail.com>
2021-07-12 19:45:16 -0300

    qs + insertion finished

--- quicksort-insercao.c
+++ quicksort-insercao.c
@@ -1,11 +1,21 @@
 #include "insercao.h"
 #include "particiona.h"
-
+#include "stdio.h"
 
 /* -------------------------------------------------------------------------- */
-/* ordena v[a..b] usando o algoritmo QuickSort com inserção e devolve v       */
+/* ordena v[a..b] usando o algoritmo QuickSort com inserï¿½ï¿½o e devolve v       */
 
-int *quicksort_insercao(int v[], int a, int b, unsigned int m) {
-  
+int *quicksort_insercao(int v[], int a, int b, unsigned int m)
+{
+  if (a < b)
+  {
+    if (b - a + 1 < m)
+      return insercao(v, a, b);
+
+    int pivot = particiona(v, a, b, v[b]);
+    quicksort_insercao(v, a, pivot - 1, m);
+    quicksort_insercao(v, pivot + 1, b, m);
+  }
+
   return v;
 }
